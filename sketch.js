e7300df7d333d8a9db88dbfbed5add5d46a7db5f
AadyaaJhunjhunwala <80698061+AadyaaJhunjhunwala@users.noreply.github.com>
2021-04-29 20:15:36 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+const Engine=Matter.Engine
+const World=Matter.World
+const Bodies=Matter.Bodies
+
+var world, engine, object
+
+function setup(){
+var canvas=createCanvas(1200,400);
+
+ engine=Engine.create();
+ world=engine.world;
+
+ object=Bodies.rectangle(200,100,50,50)
+ World.add(world,object);
+
+ console.log(object);
+}
+
+function draw()
+{background(0);
+
+  Engine.update(engine);
+  rectMode(CENTER);
+  rect(object.position.x, object.position.y,50,50);
+  
+  drawSprite();
+}
